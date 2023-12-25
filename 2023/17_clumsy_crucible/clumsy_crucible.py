d94c43558e7cde7872d79149c6b4b3ed9d161be1
Scriptim <me@scriptim.dev>
2023-12-25 17:07:43 +0100

    Add Python solution for day 17, part 2

--- 2023/17_clumsy_crucible/clumsy_crucible.py
+++ 2023/17_clumsy_crucible/clumsy_crucible.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import igraph
+from math import copysign
 
-def build_graph(heat_loss_map: [[int]]) -> igraph.Graph:
+def build_graph(heat_loss_map: [[int]], min_move: int = 1, max_move: int = 3) -> igraph.Graph:
     height = len(heat_loss_map)
     width = len(heat_loss_map[0])
 
@@ -21,14 +22,15 @@ def build_graph(heat_loss_map: [[int]]) -> igraph.Graph:
                 vertex = ((x, y), vertical)
                 vertices.append(vertex)
 
-                for delta in [1, 2, 3, -1, -2, -3]:
-                    neighbor_x, neighbor_y = (x, y + delta) if vertical else (x + delta, y)
+                for delta in (d for d in range(-max_move, max_move + 1) if abs(d) >= min_move):
+                    neighbor_x, neighbor_y = (x + delta * (not vertical), y + delta * vertical)
                     if not (0 <= neighbor_x < width and 0 <= neighbor_y < height):
                         continue
 
-                    neighbor = ((neighbor_x, neighbor_y), not vertical)
-                    edges.append((vertex, neighbor))
-                    weight = heat_loss_map[neighbor_y][neighbor_x] + (weights[-1] if abs(delta) > 1 else 0)
+                    edges.append((vertex, ((neighbor_x, neighbor_y), not vertical)))
+                    sign = int(copysign(1, delta))
+                    weight = sum(heat_loss_map[y + i * vertical][x + i * (not vertical)]
+                                 for i in range(sign, delta + sign, sign))
                     weights.append(weight)
 
     edges = map(lambda e: (vertices.index(e[0]), vertices.index(e[1])), edges)
@@ -36,13 +38,20 @@ def build_graph(heat_loss_map: [[int]]) -> igraph.Graph:
     graph.es['weight'] = weights
     return graph
 
+def shortest_path_weight(heat_loss_map: [[int]], min_move: int = 1, max_move: int = 3) -> int:
+    graph = build_graph(heat_loss_map, min_move, max_move)
+    shortest_path = graph.get_shortest_path(0, 1, weights='weight', output='epath')
+    return sum(map(lambda e: graph.es[e]['weight'], shortest_path))
+
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         # parse input
         heat_loss_map = [[int(c) for c in line.strip()] for line in f.readlines()]
 
         # part 1
-        graph = build_graph(heat_loss_map)
-        shortest_path = graph.get_shortest_path(0, 1, weights='weight', output='epath')
-        result = sum(map(lambda e: graph.es[e]['weight'], shortest_path))
+        result = shortest_path_weight(heat_loss_map)
+        print(result)
+
+        # part 2
+        result = shortest_path_weight(heat_loss_map, 4, 10)
         print(result)
