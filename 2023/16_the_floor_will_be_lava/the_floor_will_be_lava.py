61ea3f5cb990d426fa033e9eb85b5c79ee762dfe
Scriptim <me@scriptim.dev>
2023-12-25 11:37:18 +0100

    Add Python solution for day 16, part 2

--- 2023/16_the_floor_will_be_lava/the_floor_will_be_lava.py
+++ 2023/16_the_floor_will_be_lava/the_floor_will_be_lava.py
@@ -15,8 +15,8 @@ def beam_step(beam: tuple[tuple[int, int], tuple[int, int]], grid: [str]) -> [tu
         beams = [((x + dx, y + dy), (dx, dy))]
     return list(filter(lambda beam: 0 <= beam[0][0] < len(grid[0]) and 0 <= beam[0][1] < len(grid), beams))
 
-def energize(grid: [str]) -> int:
-    beams = [((0, 0), (1, 0))]
+def energize(beam: tuple[tuple[int, int], tuple[int, int]], grid: [str]) -> int:
+    beams = [beam]
     beam_history = set(beams)
     stable = False
     while not stable:
@@ -37,5 +37,16 @@ if __name__ == '__main__':
         grid = [line.strip() for line in f.readlines()]
 
         # part 1
-        result = energize(grid)
+        result = energize(((0, 0), (1, 0)), grid)
+        print(result)
+
+        # part 2
+        edges = []
+        for y in range(len(grid)):
+            edges.append(((0, y), (1, 0)))
+            edges.append(((len(grid[0]) - 1, y), (-1, 0)))
+        for x in range(len(grid[0])):
+            edges.append(((x, 0), (0, 1)))
+            edges.append(((x, len(grid) - 1), (0, -1)))
+        result = max(map(lambda edge: energize(edge, grid), edges))
         print(result)
