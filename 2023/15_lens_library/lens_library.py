ab07e567d98a1e603ce51b3c30f4e3ecb3087a5e
Scriptim <me@scriptim.dev>
2023-12-25 10:53:35 +0100

    Add Python solution for day 15, part 2

--- 2023/15_lens_library/lens_library.py
+++ 2023/15_lens_library/lens_library.py
@@ -8,6 +8,28 @@ def hash(str: str) -> int:
         value %= 256
     return value
 
+def perform_operation(operation: str, boxes: dict[int, dict[str, int]]) -> None:
+    if '=' in operation:
+        [label, focal_length] = operation.split('=')
+    else:
+        label, focal_length = operation.split('-')[0], None
+
+    box = hash(label)
+    if box not in boxes:
+        boxes[box] = {}
+
+    if focal_length is not None:
+        boxes[box][label] = int(focal_length)
+    elif label in boxes[box]:
+        del boxes[box][label]
+
+def focusing_power(boxes: dict[int, dict[str, int]]) -> int:
+    power = 0
+    for box_num, box in boxes.items():
+        for slot_num, focal_length in enumerate(box.values()):
+            power += (box_num + 1) * (slot_num + 1) * focal_length
+    return power
+
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         # parse input
@@ -15,4 +37,11 @@ if __name__ == '__main__':
 
         # part 1
         result = sum(map(hash, steps))
+        print(result)
+
+        # part 2
+        boxes = {}
+        for step in steps:
+            perform_operation(step, boxes)
+        result = focusing_power(boxes)
         print(result)
