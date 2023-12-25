1b43f7964ea9c51ef58b118687a75087d44b115e
Scriptim <me@scriptim.dev>
2023-12-25 17:28:51 +0100

    Optimize Python solution for day 17

--- 2023/17_clumsy_crucible/clumsy_crucible.py
+++ 2023/17_clumsy_crucible/clumsy_crucible.py
@@ -1,47 +1,36 @@
 #!/usr/bin/env python3
 
-import igraph
+import dijkstar
 from math import copysign
 
-def build_graph(heat_loss_map: [[int]], min_move: int = 1, max_move: int = 3) -> igraph.Graph:
+def build_graph(heat_loss_map: [[int]], min_move: int = 1, max_move: int = 3) -> dijkstar.Graph:
+    graph = dijkstar.Graph()
     height = len(heat_loss_map)
     width = len(heat_loss_map[0])
 
-    vertices = ['start', 'destination']
-    edges = [
+    for v1, v2 in [
         ('start', ((0, 0), True)),
         ('start', ((0, 0), False)),
         (((width - 1, height - 1), True), 'destination'),
         (((width - 1, height - 1), False), 'destination'),
-    ]
-    weights = [0, 0, 0, 0]
+    ]:
+        graph.add_edge(v1, v2, 0)
 
     for y in range(height):
         for x in range(width):
             for vertical in [True, False]:
-                vertex = ((x, y), vertical)
-                vertices.append(vertex)
-
-                for delta in (d for d in range(-max_move, max_move + 1) if abs(d) >= min_move):
+                for delta in list(range(min_move, max_move + 1)) + list(range(-min_move, -max_move - 1, -1)):
                     neighbor_x, neighbor_y = (x + delta * (not vertical), y + delta * vertical)
                     if not (0 <= neighbor_x < width and 0 <= neighbor_y < height):
                         continue
 
-                    edges.append((vertex, ((neighbor_x, neighbor_y), not vertical)))
                     sign = int(copysign(1, delta))
-                    weight = sum(heat_loss_map[y + i * vertical][x + i * (not vertical)]
-                                 for i in range(sign, delta + sign, sign))
-                    weights.append(weight)
+                    if abs(delta) == min_move:
+                        weight = sum(heat_loss_map[y + i * vertical][x + i * (not vertical)] for i in range(sign, min_move * sign, sign))
+                    weight += heat_loss_map[neighbor_y][neighbor_x]
+                    graph.add_edge(((x, y), vertical), ((neighbor_x, neighbor_y), not vertical), weight)
 
-    edges = map(lambda e: (vertices.index(e[0]), vertices.index(e[1])), edges)
-    graph = igraph.Graph(edges, directed=True)
-    graph.es['weight'] = weights
     return graph
-
-def shortest_path_weight(heat_loss_map: [[int]], min_move: int = 1, max_move: int = 3) -> int:
-    graph = build_graph(heat_loss_map, min_move, max_move)
-    shortest_path = graph.get_shortest_path(0, 1, weights='weight', output='epath')
-    return sum(map(lambda e: graph.es[e]['weight'], shortest_path))
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
@@ -49,9 +38,11 @@ if __name__ == '__main__':
         heat_loss_map = [[int(c) for c in line.strip()] for line in f.readlines()]
 
         # part 1
-        result = shortest_path_weight(heat_loss_map)
+        graph = build_graph(heat_loss_map)
+        result = dijkstar.find_path(graph, 'start', 'destination').total_cost
         print(result)
 
         # part 2
-        result = shortest_path_weight(heat_loss_map, 4, 10)
+        graph = build_graph(heat_loss_map, 4, 10)
+        result = dijkstar.find_path(graph, 'start', 'destination').total_cost
         print(result)
