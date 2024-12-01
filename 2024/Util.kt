1f77d1777630aeb3f0d314b48b32ad68942b09ad
Scriptim <me@scriptim.dev>
2024-12-01 09:59:33 +0100

    Add Kotlin solution for day 1, part 1

---
+++ 2024/Util.kt
@@ -0,0 +1,1 @@
+fun <T> List<T>.toPair(): Pair<T, T> = this[0] to this[1]
---
+++ 2024/day01_historian_hysteria/HistorianHysteria.kt
@@ -0,0 +1,25 @@
+package day01_historian_hysteria
+
+import toPair
+import java.io.File
+import kotlin.math.abs
+
+class HistorianHysteria {
+
+    fun readInput(): List<String> =
+        File("src/day01_historian_hysteria/input.txt").readLines()
+
+    fun parseInput(lines: List<String>): Pair<List<Int>, List<Int>> =
+        lines.map { it.split("   ").map(String::toInt).toPair() }.unzip()
+
+    fun totalListDistance(left: List<Int>, right: List<Int>): Int =
+        left.sorted().zip(right.sorted()).sumOf { (l, r) -> abs(l - r) }
+
+}
+
+fun main() {
+    HistorianHysteria().run {
+        val (left, right) = parseInput(readInput())
+        println(totalListDistance(left, right))
+    }
+}
