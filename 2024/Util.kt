00fc3ada0ad999117d80bf1ed0be9bbf936729f1
Scriptim <me@scriptim.dev>
2024-12-02 09:34:08 +0100

    Add Kotlin solution for day 2, part 2

--- 2024/Util.kt
+++ 2024/Util.kt
@@ -1,1 +1,3 @@
 fun <T> List<T>.toPair(): Pair<T, T> = this[0] to this[1]
+
+fun <T> List<T>.dropAt(index: Int): List<T> = this.take(index) + this.drop(index + 1)
--- 2024/day02_red_nosed_reports/RedNosedReports.kt
+++ 2024/day02_red_nosed_reports/RedNosedReports.kt
@@ -1,6 +1,8 @@
 package day02_red_nosed_reports
 
+import dropAt
 import java.io.File
+import kotlin.math.abs
 
 typealias Report = List<Int>
 
@@ -10,16 +12,27 @@ class RedNosedReports {
 
     fun parseInput(lines: List<String>): List<Report> = lines.map { it.split(" ").map(String::toInt) }
 
-    fun numSafe(reports: List<Report>): Int = reports.count { report ->
-        val deltas: List<Int> = report.indices.drop(1).map { i -> report[i] - report[i - 1] }
-        deltas.all { it in -3..-1 } || deltas.all { it in 1..3 }
+    private fun isSafe(report: Report, problemDampener: Boolean = false): Boolean {
+        val deltas: MutableList<Int> = report.indices.drop(1).map { i -> report[i] - report[i - 1] }.toMutableList()
+        val shouldIncrease = deltas.count { it > 0 } > deltas.count { it < 0 }
+
+        return deltas.withIndex().all { (i, d) ->
+            when {
+                abs(d) in 1..3 && (d > 0 == shouldIncrease) -> true
+                problemDampener -> isSafe(report.dropAt(i)) || isSafe(report.dropAt(i + 1))
+                else -> false
+            }
+        }
     }
+
+    fun numSafe(reports: List<Report>, problemDampener: Boolean): Int = reports.count { isSafe(it, problemDampener) }
 
 }
 
 fun main() {
     RedNosedReports().run {
         val reports = parseInput(readInput())
-        println(numSafe(reports))
+        println(numSafe(reports, false))
+        println(numSafe(reports, true))
     }
 }
