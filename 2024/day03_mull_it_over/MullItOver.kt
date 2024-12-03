d0bb861a133cde642f6cb25bcf9203701175b388
Scriptim <me@scriptim.dev>
2024-12-03 09:58:30 +0100

    Add Kotlin solution for day 3, part 2

--- 2024/day03_mull_it_over/MullItOver.kt
+++ 2024/day03_mull_it_over/MullItOver.kt
@@ -4,12 +4,30 @@ import java.io.File
 
 class MullItOver {
 
+    private val mulInstructionRegex = Regex("""mul\((\d{1,3}),(\d{1,3})\)""")
+    private val doInstructionRegex = Regex("""do(?!n't)\(\)""")
+    private val doNotInstructionRegex = Regex("""don't\(\)""")
+
     fun readInput(): String = File("src/day03_mull_it_over/input.txt").readText()
 
-    fun runProgram(memory: String): Int {
-        val mulInstructionRegex = Regex("""mul\((\d{1,3}),(\d{1,3})\)""")
-        val instructions = mulInstructionRegex.findAll(memory).toList()
-        return instructions.sumOf { it.groupValues[1].toInt() * it.groupValues[2].toInt() }
+    private fun evalMulInstruction(instruction: MatchResult): Int =
+        instruction.groupValues[1].toInt() * instruction.groupValues[2].toInt()
+
+    fun runProgram(memory: String): Int = mulInstructionRegex.findAll(memory).map(::evalMulInstruction).sum()
+
+    fun runProgramConditionally(memory: String): Int {
+        var sum = 0
+        var enabled = true
+        memory.indices.forEach {
+            when {
+                doInstructionRegex.matchesAt(memory, it) -> enabled = true
+                doNotInstructionRegex.matchesAt(memory, it) -> enabled = false
+                enabled && mulInstructionRegex.matchesAt(memory, it) ->
+                    sum += evalMulInstruction(mulInstructionRegex.find(memory, it)!!)
+            }
+        }
+
+        return sum
     }
 
 }
@@ -18,5 +36,6 @@ fun main() {
     MullItOver().run {
         val memory = readInput()
         println(runProgram(memory))
+        println(runProgramConditionally(memory))
     }
 }
