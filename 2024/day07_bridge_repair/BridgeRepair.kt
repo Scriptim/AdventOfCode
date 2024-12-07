5e74161d29394615a80ba0c7589fe3952984b84d
Scriptim <me@scriptim.dev>
2024-12-07 23:34:35 +0100

    Add Kotlin solution for day 7, part 2

--- 2024/day07_bridge_repair/BridgeRepair.kt
+++ 2024/day07_bridge_repair/BridgeRepair.kt
@@ -11,16 +11,19 @@ class BridgeRepair {
         testValue.toULong() to operands.split(" ").map(String::toULong)
     }
 
-    private fun isSatisfiable(testValue: ULong, operands: List<ULong>, accu: ULong, index: Int): Boolean {
+    private fun isSatisfiable(
+        testValue: ULong, operands: List<ULong>, accu: ULong, index: Int, concat: Boolean
+    ): Boolean {
         if (accu > testValue) return false
         if (index == operands.size) return accu == testValue
 
-        return isSatisfiable(testValue, operands, accu + operands[index], index + 1) ||
-               isSatisfiable(testValue, operands, accu * operands[index], index + 1)
+        return isSatisfiable(testValue, operands, accu + operands[index], index + 1, concat) ||
+               isSatisfiable(testValue, operands, accu * operands[index], index + 1, concat) ||
+               (concat && isSatisfiable(testValue, operands, "$accu${operands[index]}".toULong(), index + 1, true))
     }
 
-    fun totalCalibrationResult(equations: List<Pair<ULong, List<ULong>>>): ULong =
-        equations.filter { isSatisfiable(it.first, it.second, it.second.first(), 1) }.sumOf { it.first }
+    fun totalCalibrationResult(equations: List<Pair<ULong, List<ULong>>>, concat: Boolean = false): ULong =
+        equations.filter { isSatisfiable(it.first, it.second, it.second.first(), 1, concat) }.sumOf { it.first }
 
 }
 
@@ -28,6 +31,6 @@ fun main() {
     BridgeRepair().run {
         val equations = parseInput(readInput())
         println(totalCalibrationResult(equations))
+        println(totalCalibrationResult(equations, concat = true))
     }
 }
-
