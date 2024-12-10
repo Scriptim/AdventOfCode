3ba716ef67aff84c776421c8b8d492a2b4f43854
Scriptim <me@scriptim.dev>
2024-12-10 17:45:35 +0100

    Add Kotlin solution for day 10, part 2

--- 2024/day10_hoof_it/HoofIt.kt
+++ 2024/day10_hoof_it/HoofIt.kt
@@ -13,29 +13,47 @@ class HoofIt {
         line.mapIndexed { column, char -> Pair(column, row) to char.toString().toInt() }
     }.toMap()
 
-    private fun reachablePeaks(map: Map<Coordinate, Int>): Map<Coordinate, Set<Coordinate>> {
-        val peaks = map.mapValues { mutableSetOf<Coordinate>() }.toMutableMap()
+    private fun dynamicProgram(
+        map: Map<Coordinate, Int>,
+        initPeak: (peak: Coordinate) -> Unit,
+        update: (current: Coordinate, previous: Coordinate) -> Unit,
+    ) {
         val queue = ArrayDeque(map.filterValues { it == 9 }.keys)
 
         while (queue.isNotEmpty()) {
             val center = queue.removeFirst()
 
-            if (map[center] == 9) peaks[center] = mutableSetOf(center)
+            if (map[center] == 9) initPeak(center)
 
             Direction.VON_NEUMANN_NEIGHBORHOOD
                 .map { center + it }
                 .filter { it in map && map[it] == map[center]!! - 1 }
                 .forEach {
-                    peaks[it]!!.addAll(peaks[center]!!)
-                    if (it !in queue) queue.add(it)
+                    update(it, center)
+                    if (it !in queue) queue.addLast(it)
                 }
         }
-
-        return peaks.mapValues { it.value.toSet() }.toMap()
     }
 
-    fun totalTrailheadScore(map: Map<Coordinate, Int>): Int =
-        reachablePeaks(map).let { peaks -> map.filterValues { it == 0 }.keys.sumOf { peaks[it]!!.size } }
+    fun totalTrailheadScore(map: Map<Coordinate, Int>): Int {
+        val peaks = map.mapValues { mutableSetOf<Coordinate>() }.toMutableMap()
+        dynamicProgram(
+            map,
+            { peak -> peaks[peak] = mutableSetOf(peak) },
+            { current, previous -> peaks[current]!!.addAll(peaks[previous]!!) },
+        )
+        return map.filterValues { it == 0 }.keys.sumOf { peaks[it]!!.size }
+    }
+
+    fun totalTrailheadRating(map: Map<Coordinate, Int>): Int {
+        val ratings = map.mapValues { 0 }.toMutableMap()
+        dynamicProgram(
+            map,
+            { peak -> ratings[peak] = 1 },
+            { current, previous -> ratings[current] = ratings[current]!! + ratings[previous]!! },
+        )
+        return map.filterValues { it == 0 }.keys.sumOf { ratings[it]!! }
+    }
 
 }
 
@@ -43,5 +61,6 @@ fun main() {
     HoofIt().run {
         val map = parseInput(readInput())
         println(totalTrailheadScore(map))
+        println(totalTrailheadRating(map))
     }
 }
