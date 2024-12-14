221b58c59067c6e81df8dccf40381fda699a77b5
Scriptim <me@scriptim.dev>
2024-12-14 18:35:25 +0100

    Add Kotlin solution for day 14, part 1

---
+++ 2024/day14_restroom_redoubt/RestroomRedoubt.kt
@@ -0,0 +1,44 @@
+package day14_restroom_redoubt
+
+import Vector2D
+import java.io.File
+
+class RestroomRedoubt {
+
+    private val width = 101
+    private val height = 103
+    private val robotRegex = Regex("""p=(\d+),(\d+) v=(-?\d+),(-?\d+)""")
+
+    fun readInput(): List<String> = File("src/day14_restroom_redoubt/input.txt").readLines()
+
+    fun parseInput(lines: List<String>): Set<Robot> = lines.map {
+        val (xPosition, yPosition, xVelocity, yVelocity) = robotRegex.matchEntire(it)!!.destructured
+        Robot(Vector2D(xPosition.toInt(), yPosition.toInt()), Vector2D(xVelocity.toInt(), yVelocity.toInt()))
+    }.toSet()
+
+    data class Robot(val position: Vector2D<Int>, val velocity: Vector2D<Int>)
+
+    private fun move(robot: Robot, seconds: Int): Robot = robot.copy(
+        position = Vector2D(
+            (robot.position.x + robot.velocity.x * seconds).mod(width),
+            (robot.position.y + robot.velocity.y * seconds).mod(height)
+        )
+    )
+
+    fun safetyFactor(robots: Set<Robot>): Int {
+        val finalRobotsPosition = robots.map { robot -> move(robot, 100) }
+        return finalRobotsPosition.count { it.position.x < width / 2 && it.position.y < height / 2 } *
+               finalRobotsPosition.count { it.position.x < width / 2 && it.position.y > height / 2 } *
+               finalRobotsPosition.count { it.position.x > width / 2 && it.position.y > height / 2 } *
+               finalRobotsPosition.count { it.position.x > width / 2 && it.position.y < height / 2 }
+    }
+
+}
+
+fun main() {
+    RestroomRedoubt().run {
+        val robots = parseInput(readInput())
+        println(safetyFactor(robots))
+    }
+}
+
