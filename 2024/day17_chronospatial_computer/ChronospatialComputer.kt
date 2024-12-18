17f247d9399585f535c6a0242ea0d9a74fad64ea
Scriptim <me@scriptim.dev>
2024-12-18 22:22:47 +0100

    Add Kotlin solution for day 17, part 2

--- 2024/day17_chronospatial_computer/ChronospatialComputer.kt
+++ 2024/day17_chronospatial_computer/ChronospatialComputer.kt
@@ -1,19 +1,20 @@
 package day17_chronospatial_computer
 
 import java.io.File
-import kotlin.math.pow
 
 class ChronospatialComputer {
 
     fun readInput(): List<String> = File("src/day17_chronospatial_computer/input.txt").readLines()
 
     fun parseInput(lines: List<String>): Computer {
-        val (a, b, c) = lines.take(3).map { it.split(": ")[1].toInt() }
+        val (a, b, c) = lines.take(3).map { it.split(": ")[1].toULong() }
         val program = lines[4].split(": ")[1].split(",").map { it.toInt() }
         return Computer(a, b, c, program)
     }
 
-    data class Computer(var a: Int, var b: Int, var c: Int, val program: List<Int>, var instructionPointer: Int = 0) {
+    data class Computer(
+        var a: ULong, var b: ULong, var c: ULong, val program: List<Int>, var instructionPointer: Int = 0
+    ) {
 
         val halted: Boolean
             get() = instructionPointer >= program.size
@@ -21,38 +22,58 @@ class ChronospatialComputer {
         val instruction: Int
             get() = program[instructionPointer]
 
-        val operand: Int
-            get() = program[instructionPointer + 1]
+        val operand: ULong
+            get() = program[instructionPointer + 1].toULong()
 
-        val comboOperand: Int
+        val comboOperand: ULong
             get() = when (operand) {
-                4 -> a
-                5 -> b
-                6 -> c
+                4UL -> a
+                5UL -> b
+                6UL -> c
                 else -> operand
             }
 
     }
 
-    fun runProgram(computer: Computer): String = computer.copy().run {
-        val outputs = mutableListOf<Int>()
+    private fun runProgram(computer: Computer): Sequence<Int> = sequence {
+        computer.run {
+            while (!halted) {
+                when (instruction) {
+                    0 -> a = a shr comboOperand.toInt()
+                    1 -> b = b xor operand
+                    2 -> b = comboOperand and 7UL
+                    3 -> if (a > 0UL) instructionPointer = operand.toInt() - 2
+                    4 -> b = b xor c
+                    5 -> yield((comboOperand and 7UL).toInt())
+                    6 -> b = a shr comboOperand.toInt()
+                    7 -> c = a shr comboOperand.toInt()
+                }
 
-        while (!halted) {
-            when (instruction) {
-                0 -> a /= 2f.pow(comboOperand).toInt()
-                1 -> b = b xor operand
-                2 -> b = comboOperand % 8
-                3 -> if (a != 0) instructionPointer = operand - 2
-                4 -> b = b xor c
-                5 -> outputs.add(comboOperand % 8)
-                6 -> b = a / 2f.pow(comboOperand).toInt()
-                7 -> c = a / 2f.pow(comboOperand).toInt()
+                instructionPointer += 2
             }
+        }
+    }
 
-            instructionPointer += 2
+    fun programOutput(computer: Computer): String = runProgram(computer.copy()).joinToString(",")
+
+    fun findInitialAValue(computer: Computer): ULong {
+        fun dfs(outputIndex: Int, expectedA: ULong): ULong? {
+            if (outputIndex < 0) return expectedA
+
+            val expectedOutput = computer.program[outputIndex]
+
+            return (expectedA * 8UL..<(expectedA + 1UL) * 8UL).firstNotNullOfOrNull { a ->
+                val simulateComputer = computer.copy(a = a)
+                val actualOutput = runProgram(simulateComputer).first()
+                val actualA = simulateComputer.a
+
+                if (actualA == expectedA && actualOutput == expectedOutput) {
+                    dfs(outputIndex - 1, a)
+                } else null
+            }
         }
 
-        return outputs.joinToString(",")
+        return dfs(computer.program.size - 1, 0UL)!!
     }
 
 }
@@ -60,6 +81,7 @@ class ChronospatialComputer {
 fun main() {
     ChronospatialComputer().run {
         val computer = parseInput(readInput())
-        println(runProgram(computer))
+        println(programOutput(computer))
+        println(findInitialAValue(computer))
     }
 }
