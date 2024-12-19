2a594c618382202d414383b3891ad38130508fa9
Scriptim <me@scriptim.dev>
2024-12-19 19:36:39 +0100

    Add Kotlin solution for day 19, part 2

--- 2024/day19_linen_layout/LinenLayout.kt
+++ 2024/day19_linen_layout/LinenLayout.kt
@@ -18,11 +18,20 @@ class LinenLayout {
     fun countPossibleDesigns(towels: List<String>, designs: List<String>): Int =
         designs.count { designPossible(towels, it) }
 
+    fun countTotalDesignArrangements(towels: List<String>, designs: List<String>): ULong = designs.sumOf { design ->
+        val dp = mutableMapOf(0 to 1UL)
+        (1..design.length).forEach { i ->
+            dp[i] = towels.filter { design.takeLast(i).startsWith(it) }.sumOf { dp[i - it.length]!! }
+        }
+        dp[design.length]!!
+    }
+
 }
 
 fun main() {
     LinenLayout().run {
         val (towels, designs) = parseInput(readInput())
         println(countPossibleDesigns(towels, designs))
+        println(countTotalDesignArrangements(towels, designs))
     }
 }
