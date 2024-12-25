3206907b18bcd9d2cf0cd6563de073d6e64bfd70
Scriptim <me@scriptim.dev>
2024-12-25 16:23:50 +0100

    Add Kotlin solution for day 24, part 2

--- 2024/day24_crossed_wires/CrossedWires.kt
+++ 2024/day24_crossed_wires/CrossedWires.kt
@@ -24,14 +24,18 @@ class CrossedWires {
 
         fun evaluate(values: Map<String, Boolean>): Boolean = operator.evaluate(values[input1]!!, values[input2]!!)
 
+        fun isInput(): Boolean = input1.first() in "xy" || input2.first() in "xy"
+
+        fun isOutput(): Boolean = output.startsWith('z')
+
     }
 
     enum class BooleanOperator(val evaluate: (Boolean, Boolean) -> Boolean) {
         AND(Boolean::and), OR(Boolean::or), XOR(Boolean::xor)
     }
 
-    private fun evaluate(wires: Map<String, Boolean>, gates: Set<LogicGate>): Map<String, Boolean> {
-        val finalWires = wires.toMutableMap()
+    private fun evaluate(wireInputs: Map<String, Boolean>, gates: Set<LogicGate>): Map<String, Boolean> {
+        val finalWires = wireInputs.toMutableMap()
         val gateQueue = ArrayDeque(gates)
 
         while (gateQueue.isNotEmpty()) {
@@ -47,16 +51,38 @@ class CrossedWires {
         return finalWires.toMap()
     }
 
-    fun numberOutput(wires: Map<String, Boolean>, gates: Set<LogicGate>): ULong = evaluate(wires, gates)
+    fun numberOutput(wireInputs: Map<String, Boolean>, gates: Set<LogicGate>): ULong = evaluate(wireInputs, gates)
         .filterKeys { it.startsWith('z') }
         .toSortedMap(Collections.reverseOrder()).values
         .fold(0UL) { acc, value -> (acc shl 1) + if (value) 1UL else 0UL }
+
+    fun swappedGates(gates: Set<LogicGate>): String {
+        fun bit(wire: String): Int = wire.drop(1).toInt()
+        fun LogicGate.successors(): Set<LogicGate> = gates.filter { it.input1 == output || it.input2 == output }.toSet()
+        val msb = gates.filter { it.isOutput() }.maxOf { bit(it.output) }
+
+        return gates.filterNot { gate ->
+            gate.run {
+                when {
+                    isOutput() -> bit(output) == msb || operator == BooleanOperator.XOR
+                    operator == BooleanOperator.XOR -> isInput() && successors().all {
+                        it.operator in setOf(BooleanOperator.AND, BooleanOperator.XOR)
+                    }
+                    operator == BooleanOperator.AND -> (isInput() && bit(input1) == 0) || successors().all {
+                        it.operator == BooleanOperator.OR
+                    }
+                    else -> true
+                }
+            }
+        }.map { it.output }.sorted().joinToString(",")
+    }
 
 }
 
 fun main() {
     CrossedWires().run {
-        val (wires, gates) = parseInput(readInput())
-        println(numberOutput(wires, gates))
+        val (wireInputs, gates) = parseInput(readInput())
+        println(numberOutput(wireInputs, gates))
+        println(swappedGates(gates))
     }
 }
